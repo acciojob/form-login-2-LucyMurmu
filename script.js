95e7f4ccb25260facbae4ddd349ca130cc56b1ed
LucyMurmu <btech10543.22@bitmesra.ac.in>
2026-06-02 05:18:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 //your JS code here. If required.
-document.getElementById("sub").addEventListener("click" sub(){
-	alert("submited");
+document.getElementById("myform").addEventListener("submit", function(e){
+	e.preventDefault();
+	 const firstName = document.getElementById("first-name").value;
+            const lastName = document.getElementById("last-name").value;
+            const phone = document.getElementById("phone-number").value;
+            const email = document.getElementById("email-id").value;
+	alert("First Name: " + firstName +
+                " Last Name: " + lastName +
+                " Phone Number: " + phone +
+                " Email ID: " + email);
 });
